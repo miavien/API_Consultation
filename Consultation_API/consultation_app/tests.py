0e72c6942dad7fed51079439cb51b16ac8685e36
miavien <ki.llisaan@gmail.com>
2024-09-23 17:02:02 +0700

    tests for consultations

--- Consultation_API/consultation_app/tests.py
+++ Consultation_API/consultation_app/tests.py
@@ -1,3 +1,6 @@
+from datetime import time
+from http.client import responses
+
 import pytest
 from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
@@ -10,8 +13,9 @@ from django.test import TestCase
 
 # Create your tests here.
 
-User = get_user_model()
-
+@pytest.fixture
+def api_client():
+    return APIClient()
 
 @pytest.fixture
 def user_specialist(db):
@@ -21,17 +25,45 @@ def user_specialist(db):
         email='specialist@example.com',
         password='password123'
     )
-    user.is_specialist = True
+    user.role = 'Specialist'
     user.save()
     return user
 
+@pytest.fixture
+def user_client(db):
+    User = get_user_model()
+    user = User.objects.create_user(
+        username='client_user',
+        email='client@example.com',
+        password='password123'
+    )
+    user.role = 'Client'
+    user.save()
+    return user
+
+@pytest.fixture
+def authenticated_api_specialist(api_client, user_specialist):
+    api_client.force_authenticate(user=user_specialist)
+    api_client.user = user_specialist
+    return api_client
+
+@pytest.fixture
+def authenticated_api_client(api_client, user_client):
+    api_client.force_authenticate(user=user_client)
+    api_client.user = user_client
+    return api_client
+
+@pytest.fixture
+def valid_slot_data():
+    return {
+        'date': timezone.now().date() + timezone.timedelta(days=1),
+        'start_time': time(13, 0),
+        'end_time': time(13, 30),
+        'context': 'Some context here'
+    }
 
 @pytest.mark.django_db
 class TestUserRegistrationAPIView:
-
-    @pytest.fixture
-    def api_client(self):
-        return APIClient()
 
     @pytest.fixture
     def valid_user_data(self):
@@ -93,25 +125,10 @@ class TestUserRegistrationAPIView:
 @pytest.mark.django_db
 class TestCreateSlotAPIView:
 
-    @pytest.fixture
-    def api_client(self, db, user_specialist):
-        client = APIClient()
-        client.force_authenticate(user=user_specialist)
-        return client
-
-    @pytest.fixture
-    def valid_slot_data(self):
-        return {
-            'date': timezone.now().date() + timezone.timedelta(days=1),
-            'start_time': '13:00:00',
-            'end_time': '13:30:00',
-            'context': 'Some context here'
-        }
-
-    def test_create_slot_success(self, api_client, valid_slot_data):
+    def test_create_slot_success(self, authenticated_api_specialist, valid_slot_data):
         valid_slot_data.pop('context', None)
         url = reverse('create-slot')
-        response = api_client.post(url, valid_slot_data)
+        response = authenticated_api_specialist.post(url, valid_slot_data)
         assert response.status_code == status.HTTP_200_OK
         assert response.data['message'] == 'Слот успешно создан'
 
@@ -128,40 +145,94 @@ class TestCreateSlotAPIView:
         assert slot.end_time == slot_end_time
         assert slot.context is None
 
-    def test_create_slot_end_time_before_start_time(self, api_client, valid_slot_data):
+    def test_create_slot_end_time_before_start_time(self, authenticated_api_specialist, valid_slot_data):
         invalid_slot_data = valid_slot_data.copy()
         invalid_slot_data['end_time'] = '12:30:00'
         url = reverse('create-slot')
-        response = api_client.post(url, invalid_slot_data)
+        response = authenticated_api_specialist.post(url, invalid_slot_data)
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert 'detail' in response.data
-        assert response.data['detail'][0]== 'Время окончания должно быть позже времени начала'
+        assert response.data['detail'][0] == 'Время окончания должно быть позже времени начала'
 
-    def test_create_slot_invalid_date(self, api_client, valid_slot_data):
+    def test_create_slot_invalid_date(self, authenticated_api_specialist, valid_slot_data):
         invalid_slot_data = valid_slot_data.copy()
         invalid_slot_data['date'] = timezone.now().date() - timezone.timedelta(days=1)
         url = reverse('create-slot')
-        response = api_client.post(url, invalid_slot_data)
+        response = authenticated_api_specialist.post(url, invalid_slot_data)
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert 'detail' in response.data
-        assert response.data['detail'][0]== 'Дата не может быть ранее сегодняшнего дня'
+        assert response.data['detail'][0] == 'Дата не может быть ранее сегодняшнего дня'
 
-    def test_create_slot_time_overlap(self, api_client, valid_slot_data):
+    def test_create_slot_time_overlap(self, authenticated_api_specialist, valid_slot_data):
         url = reverse('create-slot')
-        response = api_client.post(url, valid_slot_data)
+        response = authenticated_api_specialist.post(url, valid_slot_data)
 
         overlapping_slot_data = valid_slot_data.copy()
         overlapping_slot_data['start_time'] = '13:15:00'
         overlapping_slot_data['end_time'] = '13:45:00'
-        response = api_client.post(url, overlapping_slot_data)
+        response = authenticated_api_specialist.post(url, overlapping_slot_data)
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert 'detail' in response.data
         assert response.data['detail'][0] == 'Время слота пересекается с другим слотом'
 
-    def test_create_slot_missing_fields(self, api_client):
+    def test_create_slot_missing_fields(self, authenticated_api_specialist):
         url = reverse('create-slot')
-        response = api_client.post(url, {})
+        response = authenticated_api_specialist.post(url, {})
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert 'date' in response.data
         assert 'start_time' in response.data
         assert 'end_time' in response.data
+
+
+@pytest.mark.django_db
+class TestSpecialistSlotListView:
+
+    def test_get_slots_success(self, authenticated_api_specialist, valid_slot_data):
+        Slot.objects.create(specialist=authenticated_api_specialist.user, **valid_slot_data)
+        url = reverse('specialist-slots')
+        response = authenticated_api_specialist.get(url, valid_slot_data)
+        assert response.status_code == status.HTTP_200_OK
+        # проверяем, что создался 1 слот
+        assert len(response.data) == 1
+
+        slot_data = response.data[0]
+        slot = Slot.objects.get(id=slot_data['id'])
+
+        # преобразовываем строки в формат datetime
+        slot_date = parse(slot_data['date']).date()
+        slot_start_time = parse(slot_data['start_time']).time()
+        slot_end_time = parse(slot_data['end_time']).time()
+
+        assert slot.date == slot_date
+        assert slot.start_time == slot_start_time
+        assert slot.end_time == slot_end_time
+
+    def test_get_slots_empty(self, authenticated_api_specialist):
+        url = reverse('specialist-slots')
+        response = authenticated_api_specialist.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == []
+
+    def test_get_slots_non_specialist(self, authenticated_api_client):
+        url = reverse('specialist-slots')
+        response = authenticated_api_client.get(url)
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+@pytest.mark.django_db
+class TestClientConsultationAPIView:
+
+    @pytest.fixture
+    def valid_slot(self, valid_slot_data, user_specialist):
+        slot_data = valid_slot_data.copy()
+        slot_data['specialist'] = user_specialist
+        slot_data['id'] = 1
+        return slot_data
+
+    def test_create_slot_success(self, authenticated_api_client, valid_slot):
+        slot = Slot.objects.create(**valid_slot)
+        url = reverse('create-consultation')
+        request_data = {'slot_id': slot.id}
+        response = authenticated_api_client.post(url, request_data)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['message'] == 'Запрос на консультацию успешно отправлен'
+        assert 'data' in response.data
